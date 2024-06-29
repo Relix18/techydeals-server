4ee3b23d0168ac15b3e8059ce5000ab197bf67d2
Relix18 <relix049@gmail.com>
2024-06-29 19:07:37 +0530

    changes

--- app.js
+++ app.js
@@ -24,12 +24,7 @@ const __dirname = dirname(__filename);
 app.use(express.static(path.resolve(__dirname, "dist")));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(
-  cors({
-    origin: [`${process.env.CLIENT_URL}`],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -6,6 +6,7 @@ const sendToken = (user, statusCode, res) => {
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
   };
 
   res.status(statusCode).cookie("token", token, option).json({
