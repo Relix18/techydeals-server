2e2a84a59fd62a490d16bfbcb329c04f7c634da5
Relix18 <relix049@gmail.com>
2024-06-29 19:18:45 +0530

    new

--- index.js
+++ index.js
@@ -28,15 +28,17 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const server = app.listen(process.env.PORT, () => {
-  console.log(`listening on port ${process.env.PORT}`);
-});
-
-process.on("unhandledRejection", (err) => {
-  console.log(`Error: ${err.message}`);
-  console.log(`Shutting down the server due to unhandled Promise Rejection`);
-
-  server.close(() => {
-    process.exit(1);
+if (!process.env.VERCEL) {
+  const server = app.listen(process.env.PORT, () => {
+    console.log(`listening on port ${process.env.PORT}`);
   });
-});
+
+  process.on("unhandledRejection", (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to unhandled Promise Rejection`);
+
+    server.close(() => {
+      process.exit(1);
+    });
+  });
+}
